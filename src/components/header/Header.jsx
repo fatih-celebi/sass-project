033ad5a8b5a82626c034992361ed5cc1339c7d7e
fatih-celebi <celebifatih.c1@gmail.com>
2023-09-05 21:39:47 +0100

    add header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.scss";
+const Header = () => {
+  return (
+    <div className="header">
+      <span className="text-shadow">Where Is Your Favourite Travel Destination?</span>
+    </div>
+  );
+};
+
+export default Header;
